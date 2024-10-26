c6103a01c50e78cd0376b3165bc3b686447419e5
Yuvraj Makwana <yuvrajmakwana100@outlook.com>
2024-10-26 18:28:35 +0530

    Update strings_slicing.py

--- Strings/strings_slicing.py
+++ Strings/strings_slicing.py
@@ -12,3 +12,18 @@ print(a[0:5])
 b = "Python"
 print(b[0])
 #it gives only one output
+
+c = "Python"
+print(c[0:3:2])
+
+#here we are starting from 0th index and ending at 3rd index but 3rd index is not included
+
+#here we are skipping 2 characters
+
+d = "Python"    
+print(d[::2])    
+
+#here we are skipping 2 characters and starting from 0th index and ending at end of string
+
+e = "Python"
+print(e[:])
