becbc20c0be9ee171f8af3d278c6eb0d817e0e7e
yuvii0125 <yuvrajmakwana100@outlook.com>
2024-10-26 18:26:58 +0530

    commit it

---
+++ Strings/negative_slicing.py
@@ -0,0 +1,25 @@
+name = "Yuvraj"
+
+
+# FOr Yuvraj we can do this
+# Y = '-6' , v = '-5' , u = '-4' , r = '-3' , a = '-2' , j = '-1'
+# as in positive slicing the index starts from 0
+#In negative slicing the index starts from -length and end with -1
+
+
+print(name[0:3])
+
+print(name[-4: -1]) 
+print(name[1:4])
+
+print(name[:4]) # is same as print(name[0:4])
+print(name[1:]) # is same as print(name[1:5])
+print(name[1:5])
+
+
+#here [1: ] means from 1st index to end
+
+#here [:4] means from 0th index to 4th index
+ 
+#negative sliciing
+
