91caa453a188be310a7d6bf68a71cca2334efde1
yuvii0125 <yuvrajmakwana100@outlook.com>
2024-10-29 00:05:07 +0530

    Inital Commit

---
+++ Dictonary and Set/dictionary_basic.py
@@ -0,0 +1,18 @@
+#Dictionaries are mutable
+#Dictionaries are unordered
+#Dictionaries are used to store key value pairs
+#Dictionaries can not contain duplicate value
+#Dictionaries are not indexed
+
+
+# Dictionary creation  {Key: Value;}
+student = {
+    "name" : "Yuvraj",
+    "age" : 21,
+    "course" : "Btech"
+}
+
+print(student['name'],type(student)) #dictionaries operated through keys
+
+
+ 
