06adad5a82f8f3a395c59c2a7042560a8e1529b9
yuvii0125 <yuvrajmakwana100@outlook.com>
2024-10-29 11:30:22 +0530

    Initial commit

--- Functions/func_with_arguments.py
+++ Functions/func_with_arguments.py
@@ -1,9 +1,9 @@
 def goodDay(name, ending="Have a nice day"):
     print("Good Day, " + name)
     print(ending)
-    return "ok" 
+    return "ok"  
 #Here we are passing 2 arguments
-                                                            
+#Here ending is default argument so if we don't pass any value it will take "Have a nice day"
 a = goodDay("Harry", "Thank you") 
 print(a)
 
