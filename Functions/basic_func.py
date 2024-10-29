f2e41eb70672eafaec27969566340c2a50852046
yuvii0125 <yuvrajmakwana100@outlook.com>
2024-10-29 11:09:52 +0530

    Initial commit

---
+++ Functions/basic_func.py
@@ -0,0 +1,34 @@
+# a = int(input("Enter your number: "))
+# b = int(input("Enter your number: "))
+# c = int(input("Enter your number: "))
+ 
+# average = (a + b + c)/3
+# print(average)
+
+# a = int(input("Enter your number: "))
+# b = int(input("Enter your number: "))
+# c = int(input("Enter your number: "))
+ 
+# average = (a + b + c)/3
+# print(average)
+  
+#If you have to do same thing repetitively then use function
+
+# Function Definition
+def avg():
+    a = int(input("Enter your number: "))
+    b = int(input("Enter your number: "))
+    c = int(input("Enter your number: "))
+    
+    average = (a + b + c)/3
+    print(average)
+
+
+avg() # Function Call
+print("Thank you!")
+avg()
+print("Thank you!")
+avg()
+print("Thank you!")
+avg()
+avg()
