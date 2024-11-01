e53d995456758e4723b9d52fb3e7610573fa0152
yuvii0125 <yuvrajmakwana100@outlook.com>
2024-11-01 18:13:51 +0530

    Initial commit

--- OOP/problem1.py
+++ OOP/problem1.py
@@ -1,1 +1,16 @@
-#
+#Create student class that can take name and marks as arguments in constructor Then create a
+#method to print the average of marks
+
+class Student:
+    def __init__(self, name,marks):
+        self.name = name
+        self.marks = marks
+
+    def average(self):
+        sum = 0
+        for i in self.marks:
+            sum += i
+        print("Average of marks is",sum/len(self.marks))
+
+s1 = Student("Yuvraj", [50,60,70])
+s1.average()
