0e1e9ee9e2ccf9962d2f463bb5dc02400fd7aab7
yuvii0125 <yuvrajmakwana100@outlook.com>
2024-11-11 14:50:16 +0530

    Initial commit

---
+++ Strings/doc_strings.py
@@ -0,0 +1,13 @@
+def greet(name):
+    """
+    Greets the user by name.
+
+    Parameters:
+    name (str): The name of the user to greet.
+
+    Returns:
+    str: A greeting message.
+    """
+    return f"Hello, {name}!"
+
+print(greet.__doc__)
