b93d4299f32aca2d6531f41fdf14f1774a221901
yuvii0125 <yuvrajmakwana100@outlook.com>
2025-02-05 13:00:24 +0530

    Python

---
+++ Exception Handling/basics.py
@@ -0,0 +1,19 @@
+num = 10
+den = 0
+# ans = num/den This will give error 
+
+#Error Handling :
+# 1. avoid shutting down the complete code
+# 2. We should deal error gracefully
+
+# There are 2 types of errors
+# 1. Syntax error : -> During Compilation
+# 2. Execution error / Runtime errors  : -> During Execution
+
+
+# Execution Error : 
+# It doesn't prevent program from running but cause it to crash it it not handled properly 
+# -> raised by python everyTime
+
+
+print("ans will give error ")
--- Exception Handling/raising_exception.py
+++
@@ -1,8 +0,0 @@
-a = int(input("Enter a first number: ")) 
-b = int(input("Enter a second  number: ")) 
-
-
-if(b==0):
-    raise ZeroDivisionError("You cannot divide by zero")
-else:
-    print(a/b)
